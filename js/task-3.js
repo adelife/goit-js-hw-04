1f8bf967f7098a0b5d161eaea2a5735d4a07cca7
adelife <142346901+adelife@users.noreply.github.com>
2023-11-16 12:36:53 +0200

    make homework

--- js/task-3.js
+++ js/task-3.js
@@ -1,21 +1,22 @@
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
 
-function checkForSpam(message) {
-   let messageLower = message.toLowerCase();
-    if (messageLower.includes(`sale`)) {
-        return true;
-    }
-    else if (messageLower.includes(`spam`)) {
-        return true;
-    }else{
-        return false;
-    }
-    
-}
+  changeUsername(newName) {
+    profile.username = newName;
+  },
+  updatePlayTime(hours) {
+    profile.playTime = profile.playTime + hours;
+  },
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
